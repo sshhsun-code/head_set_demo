4fd93a7e966630571a539c92439714292c13bf33
sunqi <iamsshhsun@gamil.com>
2020-09-15 11:10:43 +0800

    First commit

---
+++ android/app/src/main/kotlin/com/example/sunqi/head_set_plugin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.sunqi.head_set_plugin
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+
+    companion object {
+        private const val CHANNEL = "base.headset.status"
+    }
+
+    private var methodChannel: MethodChannel? = null
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+    }
+}
+
---
+++ android/app/src/main/kotlin/com/example/sunqi/head_set_plugin/headset/HeadSetManager.kt
@@ -0,0 +1,11 @@
+package com.example.sunqi.head_set_plugin.headset
+
+/**
+ * Created by sunqi13 on 2020/9/11.
+ */
+class HeadSetManager {
+
+    fun getHeadsetStatus() {
+
+    }
+}
